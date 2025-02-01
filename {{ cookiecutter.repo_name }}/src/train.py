bb71552aea0d89c243e1ca6168452b059b24e337
Viktor <isaksenviktor@gmail.com>
2025-02-01 22:14:49 +0100

    added first steps for hydra configurations

--- {{ cookiecutter.repo_name }}/src/train.py
+++ {{ cookiecutter.repo_name }}/src/train.py
@@ -1,13 +1,17 @@
 from src.model import Model
-from src.data import MyDataset
+from src.data import *
 from torch.utils.data import DataLoader
 
+
+@hydra.main(config_path="../config", config_name="train",)
 def train():
-    dataset = MyDataset()
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
     model = Model()
 
     # add rest of your training code here
+    # # Save the final trained model
+    # torch.save(
+    #     model.state_dict(), f"models/{model_save_path}/resnet18_rice_final.pth"
+    # )
 
 if __name__ == "__main__":
     train()
